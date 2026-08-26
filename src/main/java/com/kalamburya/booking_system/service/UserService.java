2a66679c473073b242c309f6bc558b5cbda9fda8
Vadim <pvkdev63@gmail.com>
2026-08-26 15:12:48 +0300

    feat: add password hashing with BCrypt
    - Add PasswordEncoderConfig with BCryptPasswordEncoder bean
    - Update UserService.createUser to hash password before saving

--- src/main/java/com/kalamburya/booking_system/service/UserService.java
+++ src/main/java/com/kalamburya/booking_system/service/UserService.java
@@ -3,6 +3,7 @@ package com.kalamburya.booking_system.service;
 import com.kalamburya.booking_system.entity.User;
 import com.kalamburya.booking_system.exception.UserNotFoundException;
 import com.kalamburya.booking_system.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,15 +12,18 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(User user) {
         if (repository.existsByEmail(user.getEmail())){
             throw new IllegalArgumentException("User with email" + user.getEmail() + "already exists");
         }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
