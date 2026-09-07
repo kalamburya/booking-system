b3348eb2e67587d2a7d6f6f55cdd8c8165f36a8e
Vadim <pvkdev63@gmail.com>
2026-09-07 20:05:32 +0300

    feat: add AccessDenied and MethodArgumentNorValid exceptions handle in GlobalExceptionHandler class

--- src/main/java/com/kalamburya/booking_system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kalamburya/booking_system/exception/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.kalamburya.booking_system.exception;
 import com.kalamburya.booking_system.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -40,6 +44,31 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResponse> handleIllegalState(IllegalStateException exc) {
         return conflictHandle(exc);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex) {
+
+        String message = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.badRequest().body(
+                new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Validation Error", message)
+        );
     }
 
     private ResponseEntity<ErrorResponse> notFoundHandle(RuntimeException exc) {
